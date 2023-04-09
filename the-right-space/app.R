a4e1884c70966e499858cdfa7bb8dfa9a0254f6f
rhondaHO <rhonda.ho.2020@scis.smu.edu.sg>
2023-04-09 18:08:41 +0800

    code draft mapping and lclq

--- the-right-space/app.R
+++ the-right-space/app.R
@@ -1,19 +1,38 @@
-library(shiny)
-
+pacman::p_load(shiny,readxl, sf, tidyverse, tmap, sfdep,  ggpubr, plotly, sfdep, data.table,leaflet)
 # for each tabPanel, can add fluidPage() to add content
 
+#import our data
+resale_sf<-readRDS("data/rds/resale_sf.rds")
+
+mpsz_sf<-readRDS("data/rds/mpsz_sf.rds")
+
+childcare_sf <- readRDS("data/rds/childcare_sf.rds")
+eldercare_sf<- readRDS("data/rds/eldercare_sf.rds")
+kindergartens_sf <- readRDS("data/rds/kindergartens_sf.rds")
+hawkercentre_new_sf <- readRDS("data/rds/hawkercentre_new_sf.rds")
+hawkercentre_healthy_sf<-readRDS("data/rds/hawkercentre_healthy_sf.rds")
+nationalparks_sf<-readRDS("data/rds/nationalparks_sf.rds")
+gyms_sf <-readRDS("data/rds/gyms_sf.rds")
+pharmacy_sf<-readRDS("data/rds/pharmacy_sf.rds")
+spf_sf<-readRDS("data/rds/spf_sf.rds")
+HDB_carpark_sf<-readRDS("data/rds/HDB_carpark_sf.rds")
+supermarket_sf<-readRDS("data/rds/supermarket_sf.rds")
+bus_stop_sf<-readRDS("data/rds/bus_stop_sf.rds")
+mrt_sf<-readRDS("data/rds/mrt_sf.rds")
+primary_school_sf<-readRDS("data/rds/primary_school_sf.rds")
+top10_primary_school_sf<-readRDS("data/rds/top10_primary_school_sf.rds")
+shopping_mall_sf<-readRDS("data/rds/shopping_mall_sf.rds")
+# combine street name and blk for HDB full address
+resale_sf$full_address <- paste("BLK", resale_sf$block, resale_sf$street_name)
 
 ui <- 
 navbarPage("the right space", 
            collapsible = TRUE,
 tabPanel("Home",),
-tabPanel("EDA",
+tabPanel("Visualisation",
          fluidPage(
-           tabsetPanel(
-             tabPanel("x"),
-             tabPanel("Chloropleth Mapping"),
-             tabPanel("Attending Workshops")
-             ))),
+           #leafletOutput("map")
+             )),
 tabPanel("Spatial Point Pattern Analysis",
          fluidPage(
            tabsetPanel(
@@ -45,8 +64,16 @@ tabPanel("Data Upload",
 )
 
 # Define server logic required to draw a histogram
+#server <- function(input, output) {
+#}
+
+# Define server logic
 server <- function(input, output) {
+  
+ 
+  
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
